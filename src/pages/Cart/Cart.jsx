515074f507efaef7f872b79db8875f0ac4f484ef
smit prajapati <smitprajapati002@gmail.com>
2024-06-25 13:34:52 -0600

    Fully environment is over

---
+++ src/pages/Cart/Cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../components/Layout/Layout'
+
+const Cart = () => {
+  return (
+    <Layout>
+      Cart
+    </Layout>
+  )
+}
+
+export default Cart
